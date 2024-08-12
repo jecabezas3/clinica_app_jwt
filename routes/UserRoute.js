c28824cdf20fda2c07d1e7c82212b3e4884de887
jhoel <jecabezas3@espe.edu.ec>
2024-08-12 08:22:48 -0500

    error de autorizacion

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -12,9 +12,9 @@ const { verifyUser, adminOnly } = require('../middleware/AuthUser');
 const router = express.Router();
 
 router.get('/users', getUsers);
-router.get('/users/:id', verifyUser, adminOnly, getUserById);
+router.get('/users/:id', getUserById);
 router.post('/users', createUser);
-router.patch('/users/:id', verifyUser, adminOnly, updateUser);
-router.delete('/users/:id', verifyUser, adminOnly, deleteUser);
+router.patch('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 module.exports = router;
