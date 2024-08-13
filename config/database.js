f4986db2e7fc0a84f5d085170387c8735b9beff4
jhoel <jecabezas3@espe.edu.ec>
2024-08-13 09:31:41 -0500

    host databse

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize('madreseg_clinica_app', 'madreseg_clinica_app_user', 'holamundo123.', {
-  host: "190.90.160.166",
+const db = new Sequelize('freedb_clinica_app', 'freedb_clinica_app_user', '9$ZkyDn8X*u@Hb%.', {
+  host: "sql.freedb.tech",
   dialect: "mysql"
 });
 
